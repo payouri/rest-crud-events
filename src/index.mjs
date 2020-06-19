8fe4cecf224b948aa8d4eddc94d7973a6a089c03
Pailloux Youri <pailloux.youri@gmail.com>
2020-06-19 13:01:39 +0200

    update dbconnect failling strat

--- src/index.mjs
+++ src/index.mjs
@@ -2,7 +2,6 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 import Koa from 'koa'
-import http from 'http'
 import { logger } from './middlewares/logger.mjs'
 import router from './routes/index.mjs'
 import { connect as DBConnect } from './_db/connect.mjs'
@@ -11,44 +10,19 @@ import bodyParser from 'koa-bodyparser'
 const onError = err => {
     console.warn(err)
 }
-
-const main = async () => {
-    http.get('http://bot.whatismyipaddress.com', (res) => {
-  const { statusCode } = res;
-  const contentType = res.headers['content-type'];
-
-  let error;
-  if (statusCode !== 200) {
-    error = new Error('Request Failed.\n' +
-                      `Status Code: ${statusCode}`);
-//   } else if (!/^application\/json/.test(contentType)) {
-    // error = new Error('Invalid content-type.\n' +
-    //                   `Expected application/json but received ${contentType}`);
-  }
-  if (error) {
-    console.error(error.message);
-    // Consume response data to free up memory
-    res.resume();
-    return;
-  }
-
-  res.setEncoding('utf8');
-  let rawData = '';
-  res.on('data', (chunk) => { rawData += chunk; });
-  res.on('end', () => {
+const tryLoop = () => {
     try {
-        console.log(rawData)
-    //   const parsedData = JSON.parse(rawData);
-    //   console.log(parsedData);
-    } catch (e) {
-      console.error(e.message);
+        await DBConnect()
+    } 
+    catch(err) {
+        console.warn(err)
+        setTimeout(tryLoop, 5000)
     }
-  });
-}).on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
-});
-    await DBConnect()
+} 
+const main = async () => {
 
+    tryLoop()
+    
     const app = new Koa()
     app.use(bodyParser())
     app.use(logger)

dbdd8986e4dd0db20773abd3c7824b7b3835ab4f
Pailloux Youri <pailloux.youri@gmail.com>
2020-06-19 13:43:10 +0200

    format

--- src/index.mjs
+++ src/index.mjs
@@ -14,48 +14,48 @@ const onError = err => {
 const tryLoop = async () => {
     try {
         await DBConnect()
-    } 
-    catch(err) {
+    }
+    catch (err) {
         console.warn(err)
         setTimeout(tryLoop, 5000)
     }
-} 
+}
 const main = async () => {
 
     http.get('bot.whatismyipaddress.com', (res) => {
         const { statusCode } = res;
         const contentType = res.headers['content-type'];
-      
+
         let error;
         if (statusCode !== 200) {
-          error = new Error('Request Failed.\n' +
-                            `Status Code: ${statusCode}`);
-        // } else if (!/^application\/json/.test(contentType)) {
-        //   error = new Error('Invalid content-type.\n' +
-        //                     `Expected application/json but received ${contentType}`);
+            error = new Error('Request Failed.\n' +
+                `Status Code: ${statusCode}`);
+            // } else if (!/^application\/json/.test(contentType)) {
+            //   error = new Error('Invalid content-type.\n' +
+            //                     `Expected application/json but received ${contentType}`);
         }
         if (error) {
-          console.error(error.message);
-          // Consume response data to free up memory
-          res.resume();
-          return;
+            console.error(error.message);
+            // Consume response data to free up memory
+            res.resume();
+            return;
         }
-      
+
         res.setEncoding('utf8');
         let rawData = '';
         res.on('data', (chunk) => { rawData += chunk; });
         res.on('end', () => {
-          try {
-            console.log(rawData)
-            // const parsedData = JSON.parse(rawData);
-            // console.log(parsedData);
-          } catch (e) {
-            console.error(e.message);
-          }
+            try {
+                console.log(rawData)
+                // const parsedData = JSON.parse(rawData);
+                // console.log(parsedData);
+            } catch (e) {
+                console.error(e.message);
+            }
         });
-      }).on('error', (e) => {
+    }).on('error', (e) => {
         console.error(`Got error: ${e.message}`);
-      });
+    });
 
     await tryLoop()
 
